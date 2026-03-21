21ea55fa35850739aa44adfeac1a3f9cea893ff0
Karthik P <padiyil2004@gmail.com>
2026-03-21 10:35:03 +0530

    installed dropdown menu of shadcn

--- app/(routes)/_common/header.tsx
+++ app/(routes)/_common/header.tsx
@@ -1,7 +1,7 @@
 import Logo from '@/components/logo';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { LoginLink } from '@kinde-oss/kinde-auth-nextjs';
+import { LoginLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import { MoonIcon, SunIcon } from 'lucide-react';
 import { useTheme } from 'next-themes'
 import Link from 'next/link';
@@ -10,6 +10,7 @@ import React from 'react'
 const Header = () => {
   const {theme,setTheme} = useTheme();
   const isDark = theme === "dark";
+  const {user} = useKindeBrowserClient();
   return (  
     <div className='sticky top-0 right-0 left-0 z-30'>
       <header className='h-16 border-b bg-background'>
@@ -20,7 +21,12 @@ const Header = () => {
               <SunIcon  className={cn("absolute size-5 transition",isDark ? "scale-100" : "scale-0")}/>
               <MoonIcon  className={cn("absolute size-5 transition",!isDark ? "scale-100" : "scale-0")}/>
             </Button>
-            <LoginLink className='bg-primary px-2 rounded-md text-foreground py-1'>Sign In</LoginLink>
+            {user ? (
+              <>
+              </>
+                ) : (
+                  <LoginLink className='bg-primary px-2 rounded-md text-foreground py-1'>Sign In</LoginLink>
+            )}
           </div>
         </div>
       </header>
