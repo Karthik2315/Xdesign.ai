bd1efefc7d3279cdfb35b469a433cf10fd2d096d
Karthik P <padiyil2004@gmail.com>
2026-03-21 15:32:42 +0530

    made a function which will give project name based on the prompt

---
+++ app/action/action.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { openrouter } from "@/lib/openrouter";
+import { generateText } from "ai";
+
+export const generateProjectName = async(prompt:string) => {
+  try {
+    const {text} = await generateText({
+      model:openrouter.chat("google/gemini-2.5-flash-lite"),
+      system: `
+        You are an AI assistant that generates very very short project names based on the user's prompt.
+        - Keep it under 5 words.
+        - Capitalize words appropriately.
+        - Do not include special characters.
+      `,
+      prompt : prompt
+    })
+    return text?.trim() || "Untitled";
+  } catch (error) {
+    console.log(error)
+  }
+}
